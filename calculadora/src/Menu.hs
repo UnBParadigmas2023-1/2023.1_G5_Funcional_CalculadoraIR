eb1d72a623935411388555360163738cf989cc6c
Kayro César <39713656+kayrocesar@users.noreply.github.com>
2023-05-01 13:18:06 -0300

    submenu de cadastro de usuario com validação

--- calculadora/src/Menu.hs
+++ calculadora/src/Menu.hs
@@ -6,8 +6,7 @@ module Menu(
 ) where
   
 import System.Console.ANSI
-
-
+import Util
 
 
 menu :: IO ()
@@ -16,15 +15,6 @@ menu = do
   putStrLn "Calculadora de imposto de renda "
   putStrLn "---------------------------------"
   putStrLn "1 - Cadastro de pessoa"
-    -- cadastro de nome
-    -- cadastro de rendimentos tributáveis totais
-    -- despesas dedutiveis:
-      -- gasto previdencia oficial:
-      -- quantos dependentes:
-      -- gasto pensao alimenticia:
-      -- gasto com saude:
-      -- gasto com educação:
-      -- Outros gastos dedutiveis (total):
   putStrLn "2 - Calculo de imposto mensal"
           -- nome da pessoa(id) : retorna o valor calculado
           -- se nao existir, perguntar se deseja cadastrar
@@ -35,7 +25,6 @@ menu = do
           -- se sim, cadastrar
   putStrLn "4 - Sair"
   escolha <- getLine
-
   case escolha of
     "1" -> submenu1
     "2" -> submenu2
@@ -43,21 +32,34 @@ menu = do
     "4" -> putStrLn "Saindo..."
     _  ->  menu
 
-submenu1 :: IO ()
+
+
+
+
 submenu1 = do
-  clearScreen
-  putStrLn "---------------------------------"
-  putStrLn "1 - "
-  putStrLn "2 - "
-  putStrLn "3 - "
-  putStrLn "4 - "
-  escolha <- getLine
-  case escolha of
-    "1" -> submenu1
-    "2" -> submenu2
-    "3" -> submenu3
-    "4" -> putStrLn "Saindo..."
-    _  ->  menu
+    clearScreen
+    putStrLn "Digite o nome: "
+    nome <- readName 
+    putStrLn "Digite o valor do rendimento tributável anual: "
+    rendimento <- readDouble
+    putStrLn "Digite o gasto anual com previdência oficial:  "
+    gastoPrevidencia <- readDouble
+    putStrLn "Digite o número de dependentes: "
+    qtdDependentes <- readInt
+    putStrLn "Digite o gasto anual com pensão alimentícia : "  
+    pensaoAlimenticia <- readDouble
+    putStrLn "Digite o gasto anual com saúde:" 
+    gastoSaude <- readDouble
+    putStrLn "Digite o gasto anual com educação: "
+    gastoEducacao <- readDouble
+    putStrLn "Digite o gasto anual total com outras despesas dedutíveis:  (Para mais informações consulte IN RFB nº 1500, de 2014)"
+    gastoOutros <- readDouble
+    -- ** aqui vai a chamar o modulo para armazenar as informações anteriores em banco de dados ou arquivo **
+    putStrLn "Informações registradas com sucesso.....Pressione qualquer tecla para voltar ao menu"
+    _ <- getLine
+    menu
+
+    
 
 submenu2 :: IO ()
 submenu2 = do
