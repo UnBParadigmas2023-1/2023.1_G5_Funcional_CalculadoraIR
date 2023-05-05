2606ac1f726beb677704f5bb68f33d4d7b7bbb2c
Lucas Lopes <38164895+lucaslop@users.noreply.github.com>
2023-05-05 19:10:00 -0300

    Correção de cálculos. Co-authored-by: Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com> Co-authored-by: Paulo Gonçalves Lima <18203100+PauloGoncalvesLima@users.noreply.github.com>

--- calculadora/src/CalculoDeImposto/Pessoa.hs
+++ calculadora/src/CalculoDeImposto/Pessoa.hs
@@ -14,6 +14,12 @@ baseCalculoImpostoSimples rendimentosTributaveis =
   if rendimentosTributaveis * 0.2 <= 16754.34 then calculaImpostoDevidoAux baseDeCalculo else
   calculaImpostoDevidoAux rendimentosTributaveis - 16754.34
 
+
+deducaoTotal :: Bool -> Float -> Float
+deducaoTotal calculoAnual deducao
+    | calculoAnual = deducao
+    | otherwise = deducao / 12.0
+
 calculaImpostoDevido :: Bool -> Titular -> Float
 calculaImpostoDevido calculoAnual pessoaParaOCalculo =
     let baseDeCalculo = (rendimentosTributaveis pessoaParaOCalculo) - (deducaoTotal calculoAnual (dedutiveis pessoaParaOCalculo)) in
@@ -23,7 +29,8 @@ calculaImpostoDevidoAux :: Float -> Float
 calculaImpostoDevidoAux baseDeCalculo
     | baseDeCalculo <= 0 = 0.0
     | baseDeCalculo <= 1903.98 = 0.0
-    | baseDeCalculo <= 2826.65 = (baseDeCalculo * 0.075 - 142.80) + calculaImpostoDevidoAux (baseDeCalculo - 2826.65)
-    | baseDeCalculo <= 3751.05 = (baseDeCalculo * 0.15 - 354.80) + calculaImpostoDevidoAux (baseDeCalculo - 3751.05)
-    | baseDeCalculo <= 4664.68 = (baseDeCalculo * 0.225 - 636.13) + calculaImpostoDevidoAux (baseDeCalculo - 4664.68)
-    | otherwise = (baseDeCalculo * 0.275 - 869.36) + calculaImpostoDevidoAux (baseDeCalculo - 4664.68)
+    | baseDeCalculo <= 2826.65 = (baseDeCalculo * 0.075 - 142.8)
+    | baseDeCalculo <= 3751.05 = (baseDeCalculo * 0.15 - 354.8)
+    | baseDeCalculo <= 4664.68 = (baseDeCalculo * 0.225 - 636.13)
+    | otherwise = (baseDeCalculo * 0.275 - 869.36)
+
