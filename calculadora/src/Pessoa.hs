047254ec9bf3819a790c849cd3c78a319ed37d50
Thalisson-Alves <thalisson2030@gmail.com>
2023-05-05 21:16:36 -0300

    Integração cálculos
    Co-authored-by: Flavio Vieira <31935777+flaviovl@users.noreply.github.com>
    Co-authored-by: Kayro César <39713656+kayrocesar@users.noreply.github.com>
    Co-authored-by: Lucas Lopes <38164895+lucaslop@users.noreply.github.com>
    Co-authored-by: 8ifq3 <github.8ifq3@slmails.com>
    Co-authored-by: Gabrielle Ribeiro <gabrielleribeiro2010@gmail.com>
    Co-authored-by: Paulo Gonçalves Lima <18203100+PauloGoncalvesLima@users.noreply.github.com>
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>
    Co-authored-by: luisgfmarques <luisgustavomarques@outlook.com>

--- calculadora/src/Pessoa.hs
+++ calculadora/src/Pessoa.hs
@@ -45,27 +45,3 @@ calculaImpostoDevidoAnualAux baseDeCalculo
   | baseDeCalculo <= 3751.05 * 12 = baseDeCalculo * 0.15 - (354.8 * 12)
   | baseDeCalculo <= 4664.68 * 12 = baseDeCalculo * 0.225 - (636.13 * 12)
   | otherwise = baseDeCalculo * 0.275 - (869.36 * 12)
-
-data Titular = Titular { 
-  rendimentosTributaveis :: Double,
-  dedutiveis :: Deducoes
-} deriving (Show)
-
-calculoSimples :: Double -> Double
-calculoSimples rendimento =
-  let limite = 16000
-    in
-      if rendimento <= limite then
-        rendimento * 0.2
-      else -- Rendimento maior que o limite.
-        3200.0
-
-calculaImpostoDevido :: Bool -> Titular -> Double
-calculaImpostoDevido calculoAnual pessoaParaOCalculo =
-    let baseDeCalculo = (rendimentosTributaveis pessoaParaOCalculo) - (deducaoTotal calculoAnual (dedutiveis pessoaParaOCalculo)) in
-    if baseDeCalculo <= 1903.98 then 0.0 else
-    if baseDeCalculo <= 2826.65 then baseDeCalculo * 0.075 - 142.80 else
-    if baseDeCalculo <= 3751.05 then baseDeCalculo * 0.15 - 354.80 else
-    if baseDeCalculo <= 4664.68 then baseDeCalculo * 0.225 -636.13 else
-    baseDeCalculo * 0.275 -869.36
-
